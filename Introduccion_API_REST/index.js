face75408bead57b300e0665045b277e83a5f452
Juan Eduardo San Martin <je.sanmartinsaezmail.com>
2023-07-03 20:48:14 -0400

    Gestión de personajes Star Wars

--- Introduccion_API_REST/index.js
+++ Introduccion_API_REST/index.js
@@ -1,8 +1,9 @@
 const http = require("http")
 const path = require("path")
 const fs = require("fs")
-const {v4: uuid} = require("uuid")
-const {validate, format} = require("rut.js")
+const { v4: uuid } = require("uuid")
+const { validate, format } = require("rut.js")
+const axios = require("axios")
 
 
 /*
@@ -25,22 +26,22 @@ const {validate, format} = require("rut.js")
 - Crear una ruta que reciba un id, con ese id consultaremos en la API de star wars y el personaje que nos devuelva
     lo registraremos en el archivo.(Se puede enviar el id por el body y por el querystring)
 - Se debe validar en el registro si el personaje ya está registrado.
-- Crear una ruta que permita visualizar los personajes ya registrado.
+- Crear una ruta que permita visualizar los personajes ya registrados.
 */
 
 http.createServer((req, res) => {
     const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`)
     const params = new URLSearchParams(searchParams)
-    
+
     //Ruta para listar personas
-    if(pathname === '/personas' && req.method === 'GET'){
+    if (pathname === '/personas' && req.method === 'GET') {
         const data = fs.readFileSync(`${__dirname}/data/personas.txt`)
         res.write(data)
         return res.end() //Finalización de la petición
     }
 
     // Ruta para registrar personas
-    if(pathname === '/personas' && req.method === 'POST'){
+    if (pathname === '/personas' && req.method === 'POST') {
         let json
         req.on("data", (datos) => {
             json = JSON.parse(datos)
@@ -48,7 +49,7 @@ http.createServer((req, res) => {
 
         return req.on("end", () => {
             json.id = uuid()
-            if(!validate(json.rut)){
+            if (!validate(json.rut)) {
                 res.write("Rut inválido por favor verificar")
                 return res.end()
             }
@@ -58,7 +59,7 @@ http.createServer((req, res) => {
             console.log(arreglo);
             //Validacion de RUT
             const busqueda = arreglo.some(persona => persona.rut === json.rut)
-            if(busqueda) {
+            if (busqueda) {
                 res.write("Este RUT ya se encuentra en nuestros registros")
                 return res.end() //Finalización de la petición
             }
@@ -71,51 +72,51 @@ http.createServer((req, res) => {
     }
 
     // Ruta para modificar persona
-    if(pathname === '/personas' && req.method === 'PUT'){
+    if (pathname === '/personas' && req.method === 'PUT') {
         let json
         req.on("data", (data) => {
             json = JSON.parse(data)
         })
         return req.on("end", () => {
-            if(json.rut) {
+            if (json.rut) {
                 res.write("No es posible editar el RUT, corregir la petición")
                 return res.end();
             }
-            if(!json.id) {
+            if (!json.id) {
                 res.write("Por favor enviar el id de la persona a modificar")
                 return res.end();
             }
-            const data = fs.readFileSync(`${__dirname}/data/personas.txt`,"utf8")
+            const data = fs.readFileSync(`${__dirname}/data/personas.txt`, "utf8")
             const arreglo = JSON.parse(data)
             const indice = arreglo.findIndex(persona => persona.id === json.id)
 
-            if(indice !== -1) {
-                arreglo[indice] = { ...arreglo[indice], ...json}
+            if (indice !== -1) {
+                arreglo[indice] = { ...arreglo[indice], ...json }
             } else {
                 res.write("Está intentado modificar un id no existente, por favor verificar")
                 return res.end()
             }
-            
+
             const texto = JSON.stringify(arreglo)
-            fs.writeFileSync(`${__dirname}/data/personas.txt`, texto,"utf8")
+            fs.writeFileSync(`${__dirname}/data/personas.txt`, texto, "utf8")
             res.write("Persona actualizada con éxito")
             return res.end()
         })
     }
 
     //Ruta para eliminar personas
-    if(pathname === '/personas' && req.method === 'DELETE') {
-        const id=params.get("id")
-        if(!id) {
+    if (pathname === '/personas' && req.method === 'DELETE') {
+        const id = params.get("id")
+        if (!id) {
             res.write("Por favor enviar el id de la persona a eliminar")
             return res.end();
         }
-        const data = fs.readFileSync(`${__dirname}/data/personas.txt`,"utf8")
+        const data = fs.readFileSync(`${__dirname}/data/personas.txt`, "utf8")
         const arreglo = JSON.parse(data)
         const indice = arreglo.findIndex(persona => persona.id == id)
 
-        if(indice != -1) {
-            arreglo.splice(indice,1)
+        if (indice != -1) {
+            arreglo.splice(indice, 1)
         } else {
             res.write("El id enviado no se encuentra registrado, por favor verificar")
             return res.end()
@@ -123,6 +124,53 @@ http.createServer((req, res) => {
         const texto = JSON.stringify(arreglo)
         fs.writeFileSync(`${__dirname}/data/personas.txt`, texto, "utf8")
         res.write("Persona eliminada con éxito")
+        return res.end()
+    }
+
+    // Agregar personajes de Star Wars
+    if (pathname === '/star-wars/agregar' && req.method === 'POST') {
+        let json
+        req.on("data", (datos) => {
+            json = JSON.parse(datos)
+        })
+        return req.on("end", async () => {
+            try {
+                if (json.id === undefined) {
+                    res.write("Por favor enviar el id del personaje")
+                    return res.end()
+                }
+                const id = json.id
+                const respuesta = await axios.get(`https://swapi.dev/api/people/${id}`)
+
+                const datos = fs.readFileSync(`${__dirname}/data/starwars.txt`, "utf8")
+                const arreglo = JSON.parse(datos)
+                const validacion = arreglo.some(personaje => personaje.url === respuesta.data.url)
+
+                if (validacion) {
+                    res.write("El personaje fue registrado previamente")
+                    return res.end()
+                }
+
+                arreglo.push(respuesta.data)
+                const texto = JSON.stringify(arreglo)
+                fs.writeFileSync(`${__dirname}/data/starwars.txt`, texto, "utf8")
+                res.write(`Personaje ${respuesta.data.name} agregado con éxito `);
+                return res.end();
+            } catch (error) {
+                if (error.response.status === 404) {
+                    res.write("Imposible conseguir los datos del personaje");
+                } else {
+                    res.write("Ocurrió un error consultando la API de Star Wars");
+                }
+                return res.end();
+            }
+        })
+    }
+
+    // Listar Personajes de Star Wars
+    if (pathname === '/star-wars/listar' && req.method === 'GET') {
+        const datos = fs.readFileSync(`${__dirname}/data/starwars.txt`, "utf8")
+        res.write(datos)
         return res.end()
     }
 
